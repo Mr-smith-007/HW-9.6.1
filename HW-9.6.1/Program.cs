417548304dd9b23ea0f4e3980d333da70f9b6e13
Алексей Репников <mr_smith_007@mail.ru>
2023-08-16 23:43:16 +0300

    Не работает

--- HW-9.6.1/Program.cs
+++ HW-9.6.1/Program.cs
@@ -1,28 +1,80 @@
 ﻿using System;
-using HW_9._6._1;
 
-class Programm
+
+
+
+namespace HW_9._6._1
 {
-    public static void Main(string[] args)
-    {
-        Exception[] exceptions = new Exception[5];
-        exceptions[0] = new MyException("Некорректный возраст");
-        exceptions[1] = new DivideByZeroException("Деление на нуль");
-        exceptions[2] = new IndexOutOfRangeException("Индекс вне заданных пределов");
-        exceptions[3] = new ArgumentOutOfRangeException("Аргумент вне заданных пределов");
-        exceptions[4] = new ArgumentNullException("Невозможно занчение null");
 
-        for (int i = 0; i < exceptions.Length; i++)
+    class Programm
+    {
+
+        public static void Main(string[] args)
         {
+            Exception[] exceptions = new Exception[5];
+            exceptions[0] = new MyException("Некорректный возраст");
+            exceptions[1] = new DivideByZeroException("Деление на нуль");
+            exceptions[2] = new IndexOutOfRangeException("Индекс вне заданных пределов");
+            exceptions[3] = new ArgumentOutOfRangeException("Аргумент вне заданных пределов");
+            exceptions[4] = new ArgumentNullException("Невозможно занчение null");
+
+            for (int i = 0; i < exceptions.Length; i++)
+            {
+                try
+                {
+                    throw exceptions[i];
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+
+            }
+
+            List<string> LNames = new List<string>();
+            LNames.Add("Иванов");
+            LNames.Add("Сидоров");
+            LNames.Add("Петров");
+            LNames.Add("Капица");
+            LNames.Add("Акимов");
+
+
+            SortLNames sortLnames = new SortLNames();
+            sortLnames.SortEvent += Show;
             try
             {
-                throw exceptions[i];
+                SortLNames.ReadSortType();
             }
-            catch (Exception e)
+            catch (FormatException ex)
             {
-                Console.WriteLine(e.ToString());
+                Console.WriteLine(ex.ToString());
             }
-            Console.ReadKey();
+
+
         }
+        static void Show(int sort);
+        {
+            switch (sort)
+            {
+                case 1:
+                    {
+                        LNames.Sort();
+                        foreach (string lname in LNames)
+                        {
+                            Console.WriteLine(lname);
+                        }
+                        break;
+                    }
+                case 2:
+    {
+        LNames.Sort();
+        LNames.Reverse();
+        foreach (string lname in LNames)
+            Console.WriteLine(lname);
+        break;
     }
+}
+        }
+
+}
 }
