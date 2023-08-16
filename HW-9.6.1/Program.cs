7fe30e2e884aa6efbd67d1198cc1f0c3710004b8
Алексей Репников <mr_smith_007@mail.ru>
2023-08-16 22:12:38 +0300

    Задание 1

--- HW-9.6.1/Program.cs
+++ HW-9.6.1/Program.cs
@@ -1,10 +1,28 @@
 ﻿using System;
-
+using HW_9._6._1;
 
 class Programm
 {
     public static void Main(string[] args)
     {
+        Exception[] exceptions = new Exception[5];
+        exceptions[0] = new MyException("Некорректный возраст");
+        exceptions[1] = new DivideByZeroException("Деление на нуль");
+        exceptions[2] = new IndexOutOfRangeException("Индекс вне заданных пределов");
+        exceptions[3] = new ArgumentOutOfRangeException("Аргумент вне заданных пределов");
+        exceptions[4] = new ArgumentNullException("Невозможно занчение null");
 
+        for (int i = 0; i < exceptions.Length; i++)
+        {
+            try
+            {
+                throw exceptions[i];
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadKey();
+        }
     }
 }
